f407f2ea3f0d82ac5527ebb4bca25b2b9645f520
Eric Vogler <ericvogler@gmail.com>
2024-04-02 12:20:38 -0700

    add Format command

--- init.lua
+++ init.lua
@@ -616,10 +616,11 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { { 'isort', 'black' } },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
+        json = { { 'prettierd', 'prettier' } },
         javascript = { { 'prettierd', 'prettier' } },
         typescript = { { 'prettierd', 'prettier' } },
       },
@@ -866,6 +867,18 @@ require('lspconfig').gleam.setup {
   end,
   -- Add additional configuration options here
 }
+
+vim.api.nvim_create_user_command('Format', function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ['end'] = { args.line2, end_line:len() },
+    }
+  end
+  require('conform').format { async = true, lsp_fallback = true, range = range }
+end, { range = true })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
